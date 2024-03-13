d22dcdc809eea824991d944143c4f194c0ec5115
Mov1ux <104890041+Mov1ux@users.noreply.github.com>
2024-03-13 15:38:05 +0000

    first commit

--- src/app.py
+++ src/app.py
@@ -1,6 +1,4 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
+
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
@@ -27,12 +25,27 @@ def sitemap():
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
 
     return jsonify(members), 200
-@app.route('/members', methods=['POST'])
+
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+    if member:
+        member_dict = {
+            "first_name": member["first_name"],
+            "id": member["id"],
+            "age": member["age"],
+            "lucky_numbers": member["lucky_numbers"]
+        }
+        return jsonify(member_dict), 200
+    else:
+        return jsonify({"error": "Member not found"}), 404
+
+@app.route('/member', methods=['POST'])
 def add_member():
     body = request.get_json()
     new_member = {
@@ -44,7 +57,13 @@ def add_member():
         }
     jackson_family.add_member(new_member)
     print(body)
-    return jsonify("member added"),200
+    return jsonify("member added"), 200
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    member = jackson_family.delete_member(id)
+    return jsonify({"done": True}), 200
+
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/datastructures.py
+++ src/datastructures.py
@@ -43,16 +43,22 @@ class FamilyStructure:
     def add_member(self, member):
         # fill this method and update the return
         self._members.append(member)
-        return "ok"
+        return None
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        for position in range(len(self._members)):
+            if self._members[position]["id"] == id:
+                self._members.pop(position)
+                return True
+            return False
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
-
+        for member in self._members:
+            if member ["id"] == id:
+                return member
+            return None
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
